f169282bc1f62c4cdd77bb65bf324754661ad74f
Juan Carlos Peña <juancarlospena@tyba.com.co>
2023-10-16 02:35:06 -0500

    Change Api

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,7 +3,7 @@ import axios, { AxiosResponse } from 'axios';
 const API_URL =
   process.env.NODE_ENV !== 'production'
     ? 'http://192.168.1.4:8080/api'
-    : 'https://faith-forge.fly.dev/api';
+    : 'https://faith-forge.onrender.com/api';
 
 export enum ApiVerbs {
   GET = 'GET',
