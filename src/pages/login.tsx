5eecdd5af20a30e4df5096f9e96c9e87ccffb5a6
Juan Carlos Peña <juancarlospena@tyba.com.co>
2023-12-14 01:06:45 -0500

    PROD

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -14,6 +14,8 @@ import { useRouter } from 'next/router';
 import { ExclamationOutlined } from '@ant-design/icons';
 import { UserLogin } from '@/redux/thunks/user/auth.thunk';
 import styled from 'styled-components';
+import { useEffect, useState } from 'react';
+import LoadingMask from '@/components/LoadingMask';
 
 const CenteredContainer = styled.div`
   display: flex;
@@ -25,57 +27,70 @@ const CenteredContainer = styled.div`
 
 const Login: NextPage = () => {
   const authSlice = useSelector((state: RootState) => state.authSlice);
+  const [initialCheckDone, setInitialCheckDone] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const router = useRouter();
   const dispatch = useDispatch<AppDispatch>();
 
   const onLogin = async (values: any) => {
+    setIsLoading(true);
     const username = values.username;
     const password = values.password;
 
     await dispatch(UserLogin({ username, password }));
 
-    if (authSlice.error || authSlice.token === '') {
-      Modal.show({
-        header: (
-          <ExclamationOutlined
-            style={{
-              fontSize: 64,
-              color: 'var(--adm-color-warning)',
-            }}
-          />
-        ),
-        title: 'Contraseña o usuario Incorrecto',
-        content: (
-          <>
-            <div>Por favor digita su usuario y contraseña correctamente</div>
-          </>
-        ),
-        closeOnMaskClick: true,
-        closeOnAction: true,
-        actions: [
-          {
-            key: 'close',
-            text: 'Cerrar',
-            primary: true,
-          },
-        ],
-      });
-      return;
-    }
-
-    router.push('/registration');
+    setInitialCheckDone(true);
+    setIsLoading(false);
   };
+
+  useEffect(() => {
+    if (initialCheckDone) {
+      if (authSlice.error || authSlice.token === '') {
+        Modal.show({
+          header: (
+            <ExclamationOutlined
+              style={{
+                fontSize: 64,
+                color: 'var(--adm-color-warning)',
+              }}
+            />
+          ),
+          title: 'Contraseña o usuario Incorrecto',
+          content: (
+            <>
+              <div>Por favor digita su usuario y contraseña correctamente</div>
+            </>
+          ),
+          closeOnMaskClick: true,
+          closeOnAction: true,
+          actions: [
+            {
+              key: 'close',
+              text: 'Cerrar',
+              primary: true,
+            },
+          ],
+        });
+        setInitialCheckDone(false);
+        return;
+      }
+
+      setInitialCheckDone(false);
+      router.push('/registration');
+    }
+  }, [authSlice.token, authSlice.error, initialCheckDone]);
 
   return (
     <CenteredContainer>
+      {isLoading ? <LoadingMask /> : ''}
       <AutoCenter>
         <Image alt="Logo Iglekdis" src={'/logo-iglekids.png'} width={350} />
       </AutoCenter>
       <Card style={{ width: '350px' }}>
         <Form
           layout="vertical"
-          onFinish={onLogin}
+          onFinish={async (values) => await onLogin(values)}
           footer={
             <Button block type="submit" color="primary" size="large">
               Iniciar sesión
