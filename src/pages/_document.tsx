0932022dbba80e4694d28b7b28ea0db6f6e3d084
Juan Carlos Peña <juancarlospena@tyba.com.co>
2023-12-13 23:25:02 -0500

    Change ENv

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-title-in-document-head */
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-const prod = process.env.NODE_ENV === 'production';
+const prod = process.env.WEB_ENV === 'production';
 
 class MyDocument extends Document {
   render() {
