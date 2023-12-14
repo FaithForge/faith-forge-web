45d0c4fac1e9ae5142edabe016e0e054e51e081b
Juan Carlos Peña <juancarlospena@tyba.com.co>
2023-12-14 01:42:47 -0500

    Cerrar sesión

--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -1,11 +1,19 @@
 import type { NextPage } from 'next';
-import { List } from 'antd-mobile';
-import { HomeOutlined, PrinterOutlined, UserOutlined } from '@ant-design/icons';
+import { List, Modal, Toast } from 'antd-mobile';
+import {
+  HomeOutlined,
+  LogoutOutlined,
+  PrinterOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
 import { useRouter } from 'next/router';
 import { Layout } from '@/components/Layout';
+import { logout } from '@/redux/slices/user/auth.slice';
+import { useDispatch } from 'react-redux';
 
 const Setting: NextPage = () => {
   const router = useRouter();
+  const dispatch = useDispatch();
   return (
     <Layout>
       <List header="Configuraciones">
@@ -32,6 +40,27 @@ const Setting: NextPage = () => {
           onClick={() => router.push('/settings/generateChurchMeetingReport')}
         >
           Generar Reporte de Servicio
+        </List.Item>
+        <List.Item
+          prefix={<LogoutOutlined />}
+          onClick={() =>
+            Modal.confirm({
+              content: '¿Desea cerrar sesión?',
+              confirmText: 'Cerrar sesión',
+              cancelText: 'Cancelar',
+              onConfirm: async () => {
+                await dispatch(logout());
+                Toast.show({
+                  icon: 'success',
+                  content: 'Se ha cerrado la sesión',
+                  position: 'bottom',
+                });
+                router.push('/login');
+              },
+            })
+          }
+        >
+          Cerrar Sesión
         </List.Item>
       </List>
     </Layout>
