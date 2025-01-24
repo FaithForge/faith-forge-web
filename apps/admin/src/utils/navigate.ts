ea2d91515fd63ffbc120603c670c8f0ffad9d013
Juan Carlos Peña <juancarlospena@tyba.com.co>
2025-01-24 17:51:43 -0500

    New Format

--- apps/admin/src/utils/navigate.ts
+++
@@ -1,4 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-export const navigateScreen = (navigation: any, screen: string) => {
-  navigation.navigate(screen);
-};
--- apps/admin/src/utils/validator.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-export const checkLastNameField = (_: any, value: string) => {
-  const lastNameArray = value.trim().split(' ');
-
-  if (lastNameArray.length >= 2) {
-    return Promise.resolve();
-  }
-  return Promise.reject();
-};
