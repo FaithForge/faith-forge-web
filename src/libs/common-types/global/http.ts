5815292fa82e6f5696913bd5c255ec65c7ab3075
Juan Carlos Peña <juancarlospena@tyba.com.co>
2025-05-21 17:59:13 -0500

    Update http.ts

--- src/libs/common-types/global/http.ts
+++ src/libs/common-types/global/http.ts
@@ -1,5 +1,5 @@
 export const API_BASE_URL =
-  process.env.NODE_ENV === 'production'
+  process.env.NODE_ENV !== 'production'
     ? 'http://faith-forge.com/api'
     : 'https://faith-forge.jucarlospm.com/api';
 
